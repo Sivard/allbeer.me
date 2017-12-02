fa34b5e2ef1fe164c68eeae5d6a809e69e9d0e44
Sivard <sekiranositel@mail.ru>
2017-12-02 16:58:44 +0300

    add delete session

--- web/router.ex
+++ web/router.ex
@@ -20,7 +20,7 @@ defmodule Allbeerme.Router do
     resources "/", BeerController
     resources "/images", ImageController
     resources "/users", UserController
-    resources "/sessions", SessionController, only: [:new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
   # Other scopes may use custom stacks.
