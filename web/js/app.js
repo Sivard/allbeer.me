01160454e444e0c466ae34f5c648f7496e572d83
Sivard <sekiranositel@mail.ru>
2017-12-03 19:16:38 +0300

    add uikit packet

--- web/js/app.js
+++ web/js/app.js
@@ -1,7 +1,14 @@
 import "phoenix_html"
 import { Socket } from "phoenix";
 
-console.log("App js loaded.");
 require("jquery");
 require("jquery-ui");
 
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+// loads the Icon plugin
+UIkit.use(Icons);
+
+// components can be called from the imported UIkit reference
+UIkit.notification('Hello world.');
